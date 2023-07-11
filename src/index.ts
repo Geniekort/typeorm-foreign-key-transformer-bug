71860c771e2e00cf1729882883a1121b3449cb57
David Kortleven <david@getplate.com>
2023-07-11 17:26:01 +0200

    Simplify script

--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,6 @@ AppDataSource.initialize().then(async () => {
     await AppDataSource.manager.save(user)
     console.log("Saved a new user with id: " + user.id)
 
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
-
-    console.log("Here you can setup and run express / fastify / any other framework.")
-
     const post = new Post()
     post.user = user
 
